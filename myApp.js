60cbbcfab5a4335e25d8039b39946068c95c6a51
Ricardo Sánchez <ricardodsanchez@users.noreply.github.com>
2021-11-07 17:25:20 -0800

    Added console.log

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+console.log("Hello World")
 
 
 
@@ -35,5 +36,4 @@ var app = express();
 
 
 
-
- module.exports = app;
+module.exports = app;
